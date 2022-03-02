1ff07b7949e0396ec05d99b9e9e40709834f200a
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-03-02 16:39:07 -0300

    feat: ao encurtar uma URL, retorna ao usuario o link novo e o antigo em JSON

--- controllers/urlController.js
+++ controllers/urlController.js
@@ -3,7 +3,8 @@ const urlService = require('../services/urlService');
 const shortenUrl = async (req, res) => {
   const { url } = req.body;
   const newUrl = await urlService.shortenUrl(url);
-  return res.status(201).json({ newUrl });
+  const formatedUrl = `${req.header('host')}/${newUrl}`;
+  return res.status(201).json({ newUrl: formatedUrl, originalUrl: url });
 }
 
 module.exports = { shortenUrl }
