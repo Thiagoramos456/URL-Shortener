5fa2e14488964df2ee74a36713517af32e47a896
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-03-02 19:55:12 -0300

    Test node_modules

--- middlewares/validateUrl.js
+++ middlewares/validateUrl.js
@@ -2,7 +2,6 @@ const URL_REGEX_PATTERN = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?
 
 const validateUrl = (req, res, next) => {
   const { body: { url } } = req;
-
   const isUrlValid = URL_REGEX_PATTERN.test(url);
   if (!isUrlValid) res.status(400).json({ error: 'Invalid URL. Try again.' })
 
