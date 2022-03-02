2e7591d8983734c8c1201021c7a9b77099ed2f35
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-03-02 15:32:16 -0300

    feat: cria funcao shortenUrl no controller

---
+++ controllers/urlController.js
@@ -0,0 +1,5 @@
+const shortenUrl = (_req, res) => {
+  return res.status(201).send('Rota Concluída')
+}
+
+module.exports = { shortenUrl }
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+require('express-async-errors');
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const { shortenUrl } = require('./controllers/urlController');
 
 app.use(bodyParser.json());
 
 const PORT = 3000;
+
+app.post('/url', shortenUrl);
 
 app.listen(PORT, () => console.log('Online'));
