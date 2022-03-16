192bb46b6e46e7313aad4b0919da001fe10dd674
Thiagoramos456 <thiagovirtual10@gmail.com>
2022-03-15 21:46:46 -0300

    feat: cria formulario para encurtar url

---
+++ client/src/components/ShortenerForm.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function shortenerForm() {
+  return (
+    <form className="flex flex-col items-center">
+      <input
+        className='rounded-full border-2 px-3 my-4 border-gray-500 text-center'
+        placeholder='Paste URL to short'
+      />
+			<button className="rounded-full bg-green-500 px-4 py-2 text-white">Short URL</button>
+    </form>
+  );
+}
+
+export default shortenerForm;
